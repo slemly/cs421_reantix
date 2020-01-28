675f1c02d0e6ad681ff525812282d378dd27b0a4
conn22 <49536271+conn22@users.noreply.github.com>
2020-01-27 18:28:38 -0800

    Reversion to previous setup

--- src/AI/heuristic.py
+++ src/AI/heuristic.py
@@ -185,7 +185,7 @@ class AIPlayer(Player):
 
         # move worker towards the tunnel or anthill if worker has food
         # DO NOT TOUCH THESE, THEY WORK RIGHT NOW! - SL
-        tunnelDist = approxDist(myWorker.coords, myInv.getTunnels().coords)
+        tunnelDist = approxDist(myWorker.coords, self.myTunnel.coords)
         hillDist = approxDist(myWorker.coords, myInv.getAnthill().coords)
         bestDist = tunnelDist - hillDist
         if (myWorker.carrying and (bestDist >= 0)):
