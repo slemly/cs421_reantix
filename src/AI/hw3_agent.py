f63572f9f0e32f0a93373327d5a6097132878f1a
lemly21 <lemly21@up.edu>
2020-02-23 11:41:10 -0800

    Small changes + documentation. moving off school machines because of spaces/tabs stuff

--- src/AI/hw3_agent.py
+++ src/AI/hw3_agent.py
@@ -114,49 +114,57 @@ class AIPlayer(Player):
         frontierNodes.append(currentStateRootNode) # append current state node to list
 
         deepestNode = 0 # depth of deepest node for flow control purposes
-    
-        #minimax algorithm
-        while(deepestNode<3):
-            nodeBestScore = HIGHCOST # arbitrary constant big int defined at top of file
-            nodeBest = None
-            #iterate through noddes in frontiernode list, find one with best score
-            
-            newFrontierNodes = []
-            for node in frontierNodes:
-                #print(str(node.depth))
-                stuff = self.expandNode(node)
-                for thing in stuff:
-                    newFrontierNodes.append(thing)
-                # newFrontierNodes.append(self.expandNode(node))
-                expandedNodes.append(node)
-                frontierNodes.remove(node)
+		#minimax algorithm
+        try:
+			while(deepestNode<3):
+				nodeBestScore = HIGHCOST # arbitrary constant big int defined at top of file
+				nodeBest = None
+				#iterate through nodes in frontiernode list, find one with best score
+				
+				newFrontierNodes = []
+				for node in frontierNodes:
+					children = self.expandNode(node)
+					for child in children:
+						newFrontierNodes.append(child)
+					# newFrontierNodes.append(self.expandNode(node))
+					expandedNodes.append(node)
+					frontierNodes.remove(node)
 
-            for node in newFrontierNodes:
-                frontierNodes.append(node)
+				for node in newFrontierNodes:
+					frontierNodes.append(node)
 
-            deepestNode += 1
-    
+				deepestNode += 1
+		
 
-        parent_dict = {} 
-        for node in newFrontierNodes:
-            if node not in parent_dict:
-                parent_dict[node.parent] = [node]
-            else:
-                parent_dict[node.parent].append(node)
+			parent_dict = {} 
+		
+			for node in newFrontierNodes:
+				if node not in parent_dict:
+					parent_dict[node.parent] = [node]
+				else:
+					parent_dict[node.parent].append(node)
 
-        for parent in parent_dict: 
-            smallChildVal = HIGHCOST 
-            bestChild = None
-            for child in parent_dict[parent]:
-                if child.maxValue < smallChildVal:
-                    smallChildVal = child.maxValue
-                    bestChild = child
-            parent.maxValue = bestChild.maxValue
-            print(parent.maxValue)
-
-
+			
+			for parent in parent_dict: 
+				smallChildVal = HIGHCOST 
+				bestChild = None
+				for child in parent_dict[parent]:
+					if child.maxValue < smallChildVal:
+						smallChildVal = child.maxValue
+						bestChild = child
+				parent.maxValue = bestChild.maxValue
+				print(parent.maxValue)
+			
+			while bestChild.depth > 1:
+				bestChild = bestChild.parent
+			print(bestChild.depth)
+				
+		except Exception as e:
+			print("Error: ", e)
+		currNode = None
 
         '''
+		
         # setting the number inside the while() conditional ajusts how deep the agent searches
         while(deepestNode<3):
             nodeBestScore = HIGHCOST # arbitrary constant big int defined at top of file
@@ -195,8 +203,10 @@ class AIPlayer(Player):
                 break
         assert (currNode.depth == 1), "Not at proper depth!" # sanity check
         '''
-        return currNode.moveToMake # return the move of the selected node
-
+        try:
+			return currNode.moveToMake # return the move of the selected node
+		except AttributeError as ae:
+			print("Working as intended","\n\n***********************\n\n",ae)
 
 
         
@@ -215,8 +225,11 @@ class AIPlayer(Player):
         return enemyLocations[0]
 
     ##
-    # TODO: Complete this function.
-    # 
+    #evaluateMaxValue
+    #Description: heuristic analysis of a game state. This is a utility function.
+    #
+    #Parameters:
+    #   currentState - A clone of the current state (GameState)
     ##
     def evaluateMaxValue(self, currentState):
         myState = currentState
@@ -480,7 +493,6 @@ class MoveNode():
         self.beta = bet
 
 
-    
     
 
 
