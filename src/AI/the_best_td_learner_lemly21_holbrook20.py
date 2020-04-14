695c80bbe96aaa8a4d3aa86b083042841c60292a
holbrook20 <holbrook20@up.edu>
2020-04-13 19:46:16 -0700

    Added expandNode(), added to categorizeState()

--- src/AI/the_best_td_learner_lemly21_holbrook20.py
+++ src/AI/the_best_td_learner_lemly21_holbrook20.py
@@ -1,6 +1,6 @@
 import random
 import sys
-sys.path.append("..")  #so other modules can be found in parent dir
+sys.path.append("..")  # so other modules can be found in parent dir
 from Player import *
 from Constants import *
 from Construction import CONSTR_STATS
@@ -15,25 +15,25 @@ from AIPlayerUtils import *
 # @author Cole Holbrook
 
 ##
-#AIPlayer
-#Description: The responsbility of this class is to interact with the game by
-#deciding a valid move based on a given game state. This class has methods that
-#will be implemented by students in Dr. Nuxoll's AI course.
+# AIPlayer
+# Description: The responsibility of this class is to interact with the game by
+# deciding a valid move based on a given game state. This class has methods that
+# will be implemented by students in Dr. Nuxoll's AI course.
 #
-#Variables:
+# Variables:
 #   playerId - The id of the player.
 ##
 class AIPlayer(Player):
 
-    #__init__
-    #Description: Creates a new Player
+    # __init__
+    # Description: Creates a new Player
     #
-    #Parameters:
+    # Parameters:
     #   inputPlayerId - The id to give the new player (int)
     #   cpy           - whether the player is a copy (when playing itself)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "Random")
+        super(AIPlayer,self).__init__(inputPlayerId, "TD-Learner")
     
 
     def categorize_state(self, currentState):
@@ -81,9 +81,8 @@ class AIPlayer(Player):
 
         currdir = os.getcwd()
         f = None
-        if(os.path.exists(os.path.join(currdir,'curr_state.csv'))):
-            f = open(os.path.exists(os.path.join(currdir,'curr_state.csv')), "r+")
-
+        if (os.path.exists(os.path.join(currdir,'curr_state.csv'))):
+            f = open(os.path.exists(os.path.join(currdir, 'curr_state.csv')), "r+")
 
         else:
             if len(myWorkers) >= len(enemyWorkers):
@@ -110,14 +109,14 @@ class AIPlayer(Player):
                 if not worker.carrying:
                     closest_food_dist = 99
                     for food in allFoods:
-                        if approxDist(worker.coords, food.cords) < closest_food_dist:
-                            closest_food_dist = approxDist(worker.coords, food.cords)
+                        if approxDist(worker.coords, food.coords) < closest_food_dist:
+                            closest_food_dist = approxDist(worker.coords, food.coords)
                     avg_worker_distance += closest_food_dist
                 else:
                     tunnel_dist = 99
                     for food in allFoods:
-                        if approxDist(worker.coords, myTunnel.cords) < tunnel_dist:
-                            tunnel_dist = approxDist(worker.coords, myTunnel.cords)
+                        if approxDist(worker.coords, myTunnel.coords) < tunnel_dist:
+                            tunnel_dist = approxDist(worker.coords, myTunnel.coords)
                     avg_worker_distance += tunnel_dist
             
             avg_worker_distance = avg_worker_distance/len(myWorkers)
@@ -169,9 +168,73 @@ class AIPlayer(Player):
             else:
                 state_scores.append(0)
 
-
             # f = open(os.path.exists(os.path.join(currdir,'curr_state.csv')), "w+")
-            pass
+
+            # ------------- Focus on getting an ant on the enemy hill --------------#
+
+            # Binary flags for worker distance from the anthill
+            enemy_hill_dist = 0
+            for worker in myWorkers:
+                enemy_hill_dist += approxDist(worker.coords, enemyHill.coords)
+            enemy_hill_dist = enemy_hill_dist/len(myWorkers)
+            if(enemy_hill_dist > 0):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 1):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 2):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 3):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 4):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 5):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 6):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 7):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            if (enemy_hill_dist > 8):
+                state_scores.append(1)
+            else:
+                state_scores.append(0)
+
+            # Flags for enemy hill health
+            enemy_hill_health = enemyHill.captureHealth
+            while (enemy_hill_health > 0):
+                state_scores.append(1)
+                enemy_hill_health -= 1
+
+            enemy_hill_health = 3 - enemy_hill_health
+            while (enemy_hill_health > 0):
+                state_scores.append(0)
+                enemy_hill_health -= 1
+
+            print(state_scores)
+            return state_scores
 
 
 
@@ -179,53 +242,53 @@ class AIPlayer(Player):
 
 
     ##
-    #getPlacement
+    # getPlacement
     #
-    #Description: called during setup phase for each Construction that
+    # Description: called during setup phase for each Construction that
     #   must be placed by the player.  These items are: 1 Anthill on
     #   the player's side; 1 tunnel on player's side; 9 grass on the
     #   player's side; and 2 food on the enemy's side.
     #
-    #Parameters:
+    # Parameters:
     #   construction - the Construction to be placed.
     #   currentState - the state of the game at this point in time.
     #
-    #Return: The coordinates of where the construction is to be placed
+    # Return: The coordinates of where the construction is to be placed
     ##
     def getPlacement(self, currentState):
         numToPlace = 0
-        #implemented by students to return their next move
-        if currentState.phase == SETUP_PHASE_1:    #stuff on my side
+        # implemented by students to return their next move
+        if currentState.phase == SETUP_PHASE_1:    # stuff on my side
             numToPlace = 11
             moves = []
             for i in range(0, numToPlace):
                 move = None
                 while move == None:
-                    #Choose any x location
+                    # Choose any x location
                     x = random.randint(0, 9)
-                    #Choose any y location on your side of the board
+                    # Choose any y location on your side of the board
                     y = random.randint(0, 3)
-                    #Set the move if this space is empty
+                    # Set the move if this space is empty
                     if currentState.board[x][y].constr == None and (x, y) not in moves:
                         move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
                         currentState.board[x][y].constr == True
                 moves.append(move)
             return moves
-        elif currentState.phase == SETUP_PHASE_2:   #stuff on foe's side
+        elif currentState.phase == SETUP_PHASE_2:   # stuff on foe's side
             numToPlace = 2
             moves = []
             for i in range(0, numToPlace):
                 move = None
                 while move == None:
-                    #Choose any x location
+                    # Choose any x location
                     x = random.randint(0, 9)
-                    #Choose any y location on enemy side of the board
+                    # Choose any y location on enemy side of the board
                     y = random.randint(6, 9)
-                    #Set the move if this space is empty
+                    # Set the move if this space is empty
                     if currentState.board[x][y].constr == None and (x, y) not in moves:
                         move = (x, y)
-                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        # Just need to make the space non-empty. So I threw whatever I felt like in there.
                         currentState.board[x][y].constr == True
                 moves.append(move)
             return moves
@@ -233,43 +296,81 @@ class AIPlayer(Player):
             return [(0, 0)]
     
     ##
-    #getMove
-    #Description: Gets the next move from the Player.
+    # getMove
+    # Description: Gets the next move from the Player.
     #
-    #Parameters:
+    # Parameters:
     #   currentState - The state of the current game waiting for the player's move (GameState)
     #
-    #Return: The Move to be made
+    # Return: The Move to be made
     ##
     def getMove(self, currentState):
         moves = listAllLegalMoves(currentState)
         selectedMove = moves[random.randint(0,len(moves) - 1)];
+        root = Node(None, currentState, 0, 0, None)
+        nodes = self.expandNode(root)
 
-        #don't do a build move if there are already 3+ ants
+        for node in nodes:
+            node.steps = self.categorize_state(node.state)
+
+        # don't do a build move if there are already 3+ ants
         numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
         while (selectedMove.moveType == BUILD and numAnts >= 3):
             selectedMove = moves[random.randint(0,len(moves) - 1)];
-            
+
         return selectedMove
+
+    ##
+    # expandNode
+    #
+    # takes in a node and expands it by
+    # taking all valid moves from that state
+    # and creating new nodes for each new move
+    #
+    # returns a list of nodes
+    ##
+    def expandNode(self, node):
+        moves = listAllLegalMoves(node.state)
+        nodes = []
+        for move in moves:
+            nextState = getNextState(node.state, move)
+            newNode = Node(move, nextState, 0, 0, node)
+            nodes.append(newNode)
+        return nodes
     
     ##
-    #getAttack
-    #Description: Gets the attack to be made from the Player
+    # getAttack
+    # Description: Gets the attack to be made from the Player
     #
-    #Parameters:
+    # Parameters:
     #   currentState - A clone of the current state (GameState)
     #   attackingAnt - The ant currently making the attack (Ant)
     #   enemyLocation - The Locations of the Enemies that can be attacked (Location[])
     ##
     def getAttack(self, currentState, attackingAnt, enemyLocations):
-        #Attack a random enemy.
+        # Attack a random enemy.
         return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
 
     ##
-    #registerWin
+    # registerWin
     #
-    # This agent doens't learn
+    # This agent doesn't learn
     #
     def registerWin(self, hasWon):
-        #method templaste, not implemented
+        # method templaste, not implemented
         pass
+
+
+##
+# Node Class
+#
+# Defines how our Node is set up to use for searching
+#
+##
+class Node:
+    def __init__(self, move, state, depth, steps, parent):
+        self.move = move
+        self.state = state
+        self.depth = depth
+        self.steps = steps + self.depth
+        self.parent = parent
